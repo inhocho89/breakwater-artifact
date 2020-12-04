569cffef7b3e944defcf8443bfd0b84e99b926a2
Inho Cho <inhocho@csail.mit.edu>
2020-12-04 02:03:48 -0500

    Support hardware timestamp

--- init_submodules.sh
+++ init_submodules.sh
@@ -9,9 +9,8 @@ git submodule update --init
 # Cloudlab patches to Shenango
 echo Applying patch to Shenango
 cd shenango
-git cherry-pick e3e92a9c2d85b0c6779744ec5042c6f9a641d51f
-git apply ../cloudlab_xl170.patch
 git apply ../connectx-4.patch
+git apply ../cloudlab_xl170.patch
 cd ..
 
 echo Done.
